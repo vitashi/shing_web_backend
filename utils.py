fa0abefe6c0f124685c73c54d81943a0f890fe1a
Vitalis Shisoka <vitalis@echomobile.org>
2017-09-21 07:32:37 +0300

    added User models and started working on apis

--- utils.py
+++ utils.py
@@ -1,4 +1,15 @@
+import os
+import hashlib
 from string import upper
 
 def to_upper(text):
 	return upper(text)
+
+
+def getSHA(pw, salt=None):
+    if not salt:
+        salt= os.urandom(32).encode('hex')
+    sha = hashlib.sha256()
+    sha.update((pw + salt).encode('utf-8'))
+    pw_sha=sha.hexdigest()
+    return salt, pw_sha
